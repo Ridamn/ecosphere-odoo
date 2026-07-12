9badbeff99d83826a257df304613b55d665f7f83
Ridamn <saxenaridam9@gmail.com>
2026-07-12 14:36:43 +0530

    Test: Add ESG score calculation validation scripts

--- test_esg.py
+++ test_esg.py
@@ -5,3 +5,17 @@ import unittest
 class TestESG(unittest.TestCase):
     def test_scores(self):
         self.assertTrue(True)
+
+    def test_emissions_calculation(self):
+        activity_amount = 500
+        emission_factor = 2.31
+        calculated = activity_amount * emission_factor
+        self.assertEqual(calculated, 1155.0)
+
+    def test_governance_clamping(self):
+        audit_score = 95.0
+        open_issues_penalty = 10.0
+        overdue_issues_penalty = 120.0
+        gov_score = audit_score - open_issues_penalty - overdue_issues_penalty
+        gov_score = max(0.0, min(100.0, gov_score))
+        self.assertEqual(gov_score, 0.0)
